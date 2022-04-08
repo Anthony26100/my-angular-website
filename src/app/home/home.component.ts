2fa93acf5932fe6d0e15a674a17d5166ab21352d
Anthony <anthony.bellier@laposte.net>
2022-04-08 13:43:45 +0200

    maj-13h

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,7 +9,30 @@ export class HomeComponent implements OnInit {
 
   constructor() { }
 
+  title ="Home";
+
+  // déclaration du tableau 
+  arrayImg = ['ciseaux.png','feuille.png','pierre.png'];
+  // Dévlaration de propriétés utilisées dans mon code
+  randomIndex = 0;
+  sourceImg = '';
+  compteur = 0;
+  
+  myAlert(msg : string){
+    alert(msg);
+  }
+
+  changeImage(){
+    // On tire un nombre aléatoire entre 0 et 2
+    this.randomIndex = Math.floor(Math.random()*3);
+    this.sourceImg = this.arrayImg[this.randomIndex];
+    this.compteur++
+    console.log('Nombre de changement de source : ' + this.compteur);
+    console.log('Nombre de changement de source : ' + this.sourceImg);
+  }
+
   ngOnInit(): void {
+     this.changeImage();
   }
 
 }
