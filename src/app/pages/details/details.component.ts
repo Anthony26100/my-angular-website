88dfabfe7926e56140bef237e04957102590feeb
Anthony <anthony.bellier@laposte.net>
2022-04-20 11:49:48 +0200

    button couleur n/b et blur

--- src/app/pages/details/details.component.ts
+++ src/app/pages/details/details.component.ts
@@ -12,10 +12,36 @@ export class DetailsComponent implements OnInit {
   constructor(public http: HttpClient, private route:ActivatedRoute) { }
   itemId ='';
   itemInfo:any;
+  imgSrc='';
+  gray= false;
+  blur = 0;
+
+  generateSrc(id=this.itemId,gray=false,blur=0){
+    this.imgSrc = 'https://picsum.photos/id/' + this.itemId + '/1280/960?'
+    this.gray? this.imgSrc +='grayscale&': null;
+    this.blur>0? this.imgSrc +='blur=' + this.blur : null;
+    // if(this.gray){
+    //   this.imgSrc +='?grayscale';
+
+    // }else{
+    //   null;
+    // }
+  }
+
+  grayscale(){
+    this.gray=!this.gray
+    this.generateSrc();
+  }
+
+  addBlur(){
+    this.blur<10?this.blur++:this.blur=0;
+    this.generateSrc();
+  }
   
   getUrl(url:string){
     return this.http.get(url);
-  }
+  };
+
 
   ngOnInit(): void {
     // On recuperer l'id dans la route active.
@@ -23,6 +49,7 @@ export class DetailsComponent implements OnInit {
     // On charge les données correspondantes à l'image.
     this.http.get('https://picsum.photos/id/'+this.itemId+'/info').subscribe( (data) =>{
       this.itemInfo = data;
+      this.generateSrc(this.itemId);
     }
     );
   }
