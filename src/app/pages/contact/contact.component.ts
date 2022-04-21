6158f554b1f6e147a19d53b5ee5cc5bc562754e1
Anthony <anthony.bellier@laposte.net>
2022-04-21 16:56:27 +0200

    commencement du formulaire avec validation du @

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -10,6 +10,35 @@ export class ContactComponent implements OnInit {
 
   constructor(public settings:SettingsService) { }
 
+  firstname = '';
+  lastname = '';
+  email = '';
+  message = '';
+
+  onSubmit(formData:any){
+    let score = 0;
+    let max = 3;
+    let errorMessage = '';
+
+    formData.firstname.length > 2 ? score++ : errorMessage += "First name is too short !\n";
+    formData.lastname.length > 2 ? score++ : errorMessage += "Last name is too short !\n";
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(formData.email)){
+       score++
+    }else{
+      errorMessage += "Email must be a valid address\n";
+    }
+
+    
+
+    let validation = score === max ? true : false;
+
+    if(validation){
+      console.log("Youpi ! Le formulaire est bien rempli ",formData);
+    }else{
+      alert('Ouuups an error occured !\n' + errorMessage)
+    }
+  }
+
   ngOnInit(): void {
     this.settings.displayCarousel= false;
   }
